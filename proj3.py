9c7992ab14dba3691f110785bdb405909b937cbc
echen200 <echen200@calpoly.edu>
2026-05-15 09:31:10 -0700

    Add files via upload

---
+++ proj3.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+from dataclasses import dataclass, field
+
+@dataclass(order=True, frozen=True)
+class Node:
+    freq: int
+    char: str
+    left: Node | None = None
+    right: Node | None  = None
+
+    def __str__(self):
+        return f"Node: {self.char}, Freq: {self.freq}"
+
+@dataclass(frozen=True)
+class MinHeap:
+    data: list[Node] = field(default_factory=list)
+
+def heapify_up(heap: MinHeap, index: int) -> MinHeap:
+
+
+def insert(heap: MinHeap, element: Node) -> MinHeap:
+
+
+def heapify_down(heap: MinHeap, index: int) -> MinHeap:
+
+
+
+def extract_min(heap: MinHeap) -> tuple[MinHeap, Node]:
+
+
+
+        
+def count_frequency(s: str)-> dict[str,int]:
+
+    pass
+
+
+def create_priority_queue(frequency: dict[str, int]) -> MinHeap:
+
+    pass
+
+
+
+def build_tree(priority_queue: MinHeap) -> Node:
+    pass
+
+
+
+
+def generate_codes(node: Node | None, prefix="", code: dict | None =None)-> dict:
+    if code is None:
+        code = {}  
+    pass
+
+
+def encode(s: str, codes: dict)-> str:
+    pass
+
+
+def decode(encoded_string: str, root: Node):
+    pass
+
+def huffman_encoding(s:str):
+    #Do Not Change this function
+    frequency = count_frequency(s)
+    pq = create_priority_queue(frequency)
+    root = build_tree_from_queue(pq)
+    codes = generate_codes(root)
+    encoded_string = encode(s, codes)
+    decoded_string = decode(encoded_string,root)
+    return encoded_string, decoded_string, codes
+
